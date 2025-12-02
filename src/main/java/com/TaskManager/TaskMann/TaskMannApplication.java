049e09e45e13082606ff4546599ecacce05afc16
Fernando <fervifane12@gmail.com>
2025-12-02 17:43:35 -0500

    Project creation

---
+++ src/main/java/com/TaskManager/TaskMann/TaskMannApplication.java
@@ -0,0 +1,13 @@
+package com.TaskManager.TaskMann;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMannApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMannApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TaskManager/TaskMann/TaskMannApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TaskManager.TaskMann;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMannApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
